601ea28e9d6fe9c208ec9ef8a1065d1b56c21459
DeMiJi <2890340612@qq.com>
2020-06-22 18:11:20 +0800

    能显示各个地区

--- app/src/main/java/com/example/coolweather/util/Utility.java
+++ app/src/main/java/com/example/coolweather/util/Utility.java
@@ -15,14 +15,14 @@ public class Utility {
      * 解析和处理服务器返回的省级数据
      */
     public static boolean handleProvinceResponse(String response) {
-        if (!TextUtils.isEmpty(response)) {
+        if (!TextUtils.isEmpty(response)) {//判断reponse字符串是否为空
             try {
-                JSONArray allProvinces = new JSONArray(response);
-                for (int i = 0; i < allProvinces.length(); i++) {
-                    JSONObject provinceObject = allProvinces.getJSONObject(i);
-                    Province province = new Province();
-                    province.setProvinceName(provinceObject.getString("name"));
-                    province.setProvinceCode(provinceObject.getInt("id"));
+                JSONArray allProvinces = new JSONArray(response);//将response生成json的数组，用  [ { } , { } , ......  , { } ]  来表示
+                for (int i = 0; i < allProvinces.length(); i++) {//历遍json数组
+                    JSONObject provinceObject = allProvinces.getJSONObject(i);//用 {  }来表示单个的数据
+                    Province province = new Province();//实例化省
+                    province.setProvinceName(provinceObject.getString("name"));//使用JSONObject解析
+                    province.setProvinceCode(provinceObject.getInt("id"));//使用LitePal添加数据
                     province.save();
                 }
                 return true;
